a932c16021622c9803d37efdcdcd993168849a8f
Marcin Ogar <marcin.ogar@gmail.com>
2023-05-30 12:59:56 +0200

    Workflow set-up complete

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <h1>{{ greeting }}</h1>
+  <div class="wrapper">
+    <Card />
+  </div>
 </template>
 
 <script lang="ts">
+import Card from "./components/Card.vue";
+
 export default {
   name: "App",
+
+  components: { Card },
+
   data() {
     return {
       greeting: "Howdy!",
@@ -14,5 +21,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./styles.scss";
+@import "./styles/index.scss";
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>How did we do?</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Card",
+};
+</script>
