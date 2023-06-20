593f2633469a57d493696ee0ce591a423288a823
Marcin Ogar <marcin.ogar@gmail.com>
2023-06-20 10:28:27 +0200

    Build and readme - complete

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,17 @@
+const currentTask = process.env.npm_lifecycle_event;
+
 const webpack = require("webpack");
 const path = require("path");
 const fse = require("fs-extra");
 const { rimrafSync } = require("rimraf");
 const { VueLoaderPlugin } = require("vue-loader");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 class RunBeforeCompile {
   apply(compiler) {
-    compiler.hooks.beforeCompile.tap("Clean dist", function () {
-      rimrafSync("./dist");
+    compiler.hooks.beforeCompile.tap("Clean docs", function () {
+      rimrafSync("./docs");
     });
   }
 }
@@ -16,19 +19,21 @@ class RunBeforeCompile {
 class RunAfterCompile {
   apply(compiler) {
     compiler.hooks.done.tap("Copy assets", function () {
-      fse.copySync("./src/assets", "./dist/assets");
+      fse.copySync("./src/assets", "./docs/assets");
     });
   }
 }
 
-module.exports = {
+let scssRule = {
+  test: /\.scss$/,
+  use: ["css-loader", "sass-loader"],
+};
+
+let config = {
   entry: "./src/index.ts",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   module: {
     rules: [
+      scssRule,
       {
         test: /\.ts$/,
         loader: "ts-loader",
@@ -38,10 +43,6 @@ module.exports = {
       {
         test: /\.vue$/,
         loader: "vue-loader",
-      },
-      {
-        test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
@@ -62,13 +63,42 @@ module.exports = {
     new RunBeforeCompile(),
     new RunAfterCompile(),
   ],
-  mode: "development",
-  devServer: {
+};
+
+if (currentTask === "dev") {
+  config.mode = "development";
+
+  scssRule.use.unshift("vue-style-loader");
+
+  config.output = {
+    filename: "main.js",
+    path: path.resolve(__dirname, "docs"),
+  };
+
+  config.devServer = {
     port: 3050,
-    static: "./dist",
+    static: "./docs",
     hot: true,
     watchFiles: "./src/index-template.html",
     historyApiFallback: true,
     open: true,
-  },
-};
+  };
+}
+
+if (currentTask === "build") {
+  config.mode = "production";
+
+  scssRule.use.unshift(MiniCssExtractPlugin.loader);
+
+  config.plugins.push(
+    new MiniCssExtractPlugin({ filename: "styles.[chunkhash].css" })
+  );
+
+  config.output = {
+    filename: "[name].[chunkhash].js",
+    chunkFilename: "[name].[chunkhash].js",
+    path: path.resolve(__dirname, "docs"),
+  };
+}
+
+module.exports = config;
